34748116a994bdcfc0cc4793104011c76dd469fd
Valentin Misiaszek <mowerv6@gmail.com>
2019-09-20 01:02:40 +0200

    Add orders models

---
+++ models/payedOrder.js
@@ -0,0 +1,31 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+// Create model and his schema
+const PayedOrder = mongoose.model(
+  "PayedOrder",
+  new mongoose.Schema({
+    products: {
+      type: Array,
+      minlength: 1
+    },
+    totalPrice: {
+      type: Number,
+      min: 0
+    },
+    date: moment().toJSON()
+  })
+);
+
+// Validation
+function validatePayedOrder(payedOrder) {
+  const schema = {
+    payedOrder: Joi.array()
+  };
+
+  return Joi.validate(payedOrder, schema);
+}
+
+exports.PayedOrder = PayedOrder;
+exports.validate = validatePayedOrder;
