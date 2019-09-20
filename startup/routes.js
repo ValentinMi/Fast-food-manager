834871feecd3d90e39b0709c516ca3406c9d7d63
Valentin Misiaszek <mowerv6@gmail.com>
2019-09-20 18:28:16 +0200

    add auth routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const products = require("../routes/products");
+const auth = require("../routes/auth");
 const users = require("../routes/users");
+const products = require("../routes/products");
 const pendingOrders = require("../routes/pendingOrders");
 const payedOrders = require("../routes/payedOrders");
 const error = require("../middlewares/error");
 
 module.exports = function(app) {
   app.use(express.json());
-  app.use("/api/products", products);
   app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use("/api/products", products);
   app.use("/api/pendingOrders", pendingOrders);
   app.use("/api/payedOrders", payedOrders);
   app.use(error);
