79b32effe99836170d04e1d5308dc3b9503a9c27
Valentin Misiaszek <mowerv6@gmail.com>
2019-09-20 18:18:09 +0200

    add winston

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const winston = require("winston");
+const config = require("config");
 const express = require("express");
 const app = express();
+
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+
 const port = 5000;
+const server = app.listen(port, () =>
+  winston.info(`Example app listening on port ${port}!`)
+);
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = server;
