b9de048ac95594af98885b3e30e2dc25150598bd
Valentin Misiaszek <mowerv6@gmail.com>
2019-09-22 21:29:14 +0200

    add redux persist

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,25 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import localForage from "localforage";
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware";
 import reducers from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const middlewares = [promise, thunk]
 
-export const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middlewares)));
+const persistConfig = {
+  key: "root",
+  storage: localForage,
+  blacklist: []
+};
+
+export const store = createStore(
+  persistReducer(persistConfig, reducers),
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(...middlewares))
+    : compose(applyMiddleware(...middlewares))
+);
+
+export const persistor = persistStore(store);
 
