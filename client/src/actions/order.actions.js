ed63fe7ee82a8e8f0451c007fcdbf502a945f098
Valentin Misiaszek <mowerv6@gmail.com>
2019-09-22 21:56:35 +0200

    Write order const

--- client/src/actions/order.actions.js
+++ client/src/actions/order.actions.js
@@ -1,31 +1,3 @@
 import * as orderConst from "../const/order.const";
 
-export const addProductToOrder = (product, quantity, price) => ({
-  type: orderConst.ADD_PRODUCT_TO_ORDER,
-  payload: {
-    product,
-    quantity,
-    price
-  }
-});
 
-export const removeProductFromOrder = index => ({
-  type: orderConst.REMOVE_PRODUCT_FROM_ORDER,
-  payload: {
-    index
-  }
-});
-
-export const buyOrder = orderList => ({
-  type: orderConst.BUY_ORDER,
-  payload: {
-    orderList
-  }
-});
-
-export const sendOrder = index => ({
-  type: orderConst.SEND_ORDER,
-  payload: {
-    index
-  }
-});
