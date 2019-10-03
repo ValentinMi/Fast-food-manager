85106306efd382150d76b5e9dcd95bc02cf5c542
Valentin Misiaszek <mowerv6@gmail.com>
2019-10-03 14:06:08 +0200

    add User populate script

--- seed.js
+++ seed.js
@@ -1,7 +1,9 @@
 const { Product } = require("./models/product");
 const { PayedOrder } = require("./models/payedOrder");
+const { User } = require("./models/user");
 const mongoose = require("mongoose");
 const moment = require("moment");
+const bcrypt = require("bcrypt");
 const config = require("config");
 
 const productsData = [
@@ -51,17 +53,42 @@ const payedOrderData = [
   }
 ];
 
+async function createUsers() {
+  const salt = await bcrypt.genSalt(10);
+
+  // Create 2 users: 1 customer & 1 admin with "password" as password
+  const users = [
+    new User({
+      email: "test@test.fr",
+      password: await bcrypt.hash("password", salt),
+      registerDate: moment().toJSON()
+    }),
+    new User({
+      email: "admin@admin.fr",
+      password: await bcrypt.hash("password", salt),
+      registerDate: moment().toJSON(),
+      isAdmin: true
+    })
+  ];
+
+  return users;
+}
+
 async function seed() {
   await mongoose.connect(config.get("db"), {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
 
+  // Clear DB
   await Product.deleteMany({});
   await PayedOrder.deleteMany({});
+  await User.deleteMany({});
 
+  // Populate DB
   await Product.insertMany(productsData);
   await PayedOrder.insertMany(payedOrderData);
+  await User.insertMany(await createUsers());
 
   mongoose.disconnect();
 
